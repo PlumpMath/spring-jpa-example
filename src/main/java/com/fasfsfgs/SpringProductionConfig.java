3cdce71a007b695664252cfada39371300c68ce4
fasfsfgs <fasfsfgs@gmail.com>
2016-04-18 10:35:11 -0300

    separate common spring config for test and production;

--- src/main/java/com/fasfsfgs/SpringProductionConfig.java
+++ src/main/java/com/fasfsfgs/SpringProductionConfig.java
@@ -1,17 +1,15 @@
 package com.fasfsfgs;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
 
 @Configuration
-@ComponentScan
+@Import(SpringConfig.class)
 @ImportResource("classpath:/com/fasfsfgs/entitymanagerfactory.production.xml")
-@EnableTransactionManagement
 public class SpringProductionConfig {
 
   @Bean
